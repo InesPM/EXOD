96142dfabe73a3d34632e01d6e4b2b06f1515b02
Inés <29770213+InesPM@users.noreply.github.com>
2019-04-11 12:29:03 +0200

    Update filtering.sh

--- bash/filtering.sh
+++ bash/filtering.sh
@@ -4,95 +4,101 @@
 #                                                                              #
 # EXOD - EPIC-pn XMM-Newton Outburst Detector                                  #
 #                                                                              #
-# Events file generation and variability computation                           #
+# Events file filtering                                                        #
 #                                                                              #
 # Inés Pastor Marazuela (2019) - ines.pastor.marazuela@gmail.com               #
 #                                                                              #
 ################################################################################
 
-# bash script generating the filtered events file and the GTI file and computing the variability.
+###
+# Parsing arguments                                                            #
+###
 
-# bash variabilitectron.sh <FOLDER> <DL> <TW> <GTR>
+# Default variables
+RATE=0.5
+FOLDER=/mnt/data/Ines/DR5
 
-################################################################################
-#                                                                              #
-# Defining the functions                                                       #
-#                                                                              #
-################################################################################
+# Input variables
+while [[ $# -gt 0 ]]; do
+case "$1" in
+  -f|--folder)           FOLDER=${2:-$FOLDER}
+  shift; shift ;;
+  -o|-obs|--observation) OBS=${2}
+  shift; shift ;;
+  -r|--rate)             RATE=${2:-$RATE}
+  shift; shift ;;
+esac
+done
 
-# Style functions
-################################################################################
+path=$FOLDER/$OBS
+
+###
+# Defining functions
+###
 
 Title(){
-  message=$1
-	i=0; x='===='
-	while [[ i -lt ${#message} ]]; do x='='$x; ((++i)); done
+  message=$1; i=0; x='===='
+  while [[ i -lt ${#message} ]]; do x='='$x; ((++i)); done
   echo -e "\n\t  $message \n\t$x"
 }
 
 title(){
-  message=$1
-	i=0; x=----
-	while [[ i -lt ${#message} ]]; do x=-$x; ((++i)); done
+  message=$1; i=0; x=----
+  while [[ i -lt ${#message} ]]; do x=-$x; ((++i)); done
   echo -e "\n\t  $message \n\t$x"
 }
 
-# Data analysis functions
+################################################################################
+#                                                                              #
+# Main programme                                                               #
+#                                                                              #
 ################################################################################
 
-preliminaries(){
+Title "Filtering observation $OBS"
 
-	title "Preliminaries"
-	path=$1/$2
-	obs=$2
+###
+# Preliminaries
+###
 
-	# Setting up SAS
-	cd $path
-	export SAS_ODF=$path
-	export SAS_CCF=$path/ccf.cif
-	export HEADAS=/usr/local/heasoft-6.22.1/x86_64-unknown-linux-gnu-libc2.19/
-	. $HEADAS/headas-init.sh
-	. /usr/local/SAS/xmmsas_20170719_1539/setsas.sh
+title "Preliminaries"
 
-	if [ ! -f $path/ccf.cif ]; then cifbuild; else echo "CIF already built"; fi
-	#if [ ! -f $path/*SUM.SAS ]; then odfingest; else echo "ODF already ingested"; fi
-	#if [ ! -f $(ls $path/*ImagingEvts*) ]; then epproc; else echo "EP already processed"; fi
+# Setting up SAS
+cd $path
+export SAS_ODF=$path
+export SAS_CCF=$path/ccf.cif
+export SAS_CCFPATH=/home/ines/Downloads/xmmsas_20180620_1732/ccf/
+export HEADAS=/home/ines/Downloads/heasoft-6.25/x86_64-pc-linux-gnu-libc2.27
+. $HEADAS/headas-init.sh
+. /home/ines/Downloads/xmmsas_20180620_1732/setsas.sh
 
-}
+cifbuild
 
-filtering(){
+###
+# Filtering
+###
 
-	title "Cleaning events file"
-	path=$1/$2
-	obs=$2
-  #rate=0.5   #$3
+title "Cleaning events file"
 
-	# File names
-	org_file=$(ls /mnt/xmmcat/3xmm_pievli/*$obs*PIEVLI*)
-	events_file=$path/PN_clean.fits
-	gti_file=$path/PN_gti.fits
-	rate_file=$path/PN_rate.fits
+# File names
+org_file=$(ls $path/*$OBS*PIEVLI*)
+events_file=$path/PN_clean.fits
+gti_file=$path/PN_gti.fits
+rate_file=$path/PN_rate.fits
 
-	# Creating GTI
-	evselect table=$org_file withrateset=Y rateset=$rate_file maketimecolumn=Y timebinsize=100 makeratecolumn=Y expression='#XMMEA_EP && (PI in [10000:12000]) && (PATTERN==0)' -V 0
+# Creating GTI
+evselect table=$org_file withrateset=Y rateset=$rate_file maketimecolumn=Y timebinsize=100 makeratecolumn=Y expression='#XMMEA_EP && (PI in [10000:12000]) && (PATTERN==0)' -V 0
 
-	fv $rate_file &
-	read -p "Rate : " rate
-	echo "Creating Good Time Intervals with threshold RATE=$rate"
+if [[ $RATE != [0-9]* ]]; then
+  echo "Opening PN_rate.fits" 
+  fv $rate_file &
+  read -p "Choose the GTI cut rate : " RATE
+fi
+echo "Creating Good Time Intervals with threshold RATE=$RATE"
 
-	tabgtigen table=$rate_file expression="RATE<=$rate" gtiset=$gti_file -V 0
+tabgtigen table=$rate_file expression="RATE<=$RATE" gtiset=$gti_file -V 0
 
-	# Cleaning events file
-	evselect table=$org_file withfilteredset=Y filteredset=$events_file destruct=Y keepfilteroutput=T expression="#XMMEA_EP && gti($gti_file,TIME) && (PATTERN<=4) && (PI in [500:12000])" -V 0
+# Cleaning events file
+evselect table=$org_file withfilteredset=Y filteredset=$events_file destruct=Y keepfilteroutput=T expression="#XMMEA_EP && gti($gti_file,TIME) && (PATTERN<=4) && (PI in [500:12000])" -V 0
 
-	echo > $path/PN_processing.log "Rate: $rate"
-}
-
-# Main programme
-
-FOLDER=$1
-obs=$2
-
-# Filtering observations
-preliminaries $FOLDER $obs
-filtering $FOLDER $obs
+echo > $path/PN_processing.log "Rate: $RATE"
+echo "The end" date 
