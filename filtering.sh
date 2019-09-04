bf33bf78c43c0297fd752e24348d72b8a138c8c5
Inés Pastor Marazuela <29770213+InesPM@users.noreply.github.com>
2019-09-04 15:19:26 +0200

    Creating PN_image.fits with filtering.sh

--- filtering.sh
+++ filtering.sh
@@ -30,6 +30,11 @@ case "$1" in
 esac
 done
 
+echo -e "\tFOLDER      = ${FOLDER}"
+echo -e "\tOBSERVATION = ${OBS}"
+echo -e "\tRATE        = ${RATE}"
+
+
 path=$FOLDER/$OBS
 
 ###
@@ -66,10 +71,9 @@ title "Preliminaries"
 cd $path
 export SAS_ODF=$path
 export SAS_CCF=$path/ccf.cif
-export SAS_CCFPATH=/opt/ccfs
-export HEADAS=/usr/local/heasoft-6.22.1/x86_64-unknown-linux-gnu-libc2.19/
+export HEADAS=/home/ines/astrosoft/heasoft-6.25/x86_64-pc-linux-gnu-libc2.27
 . $HEADAS/headas-init.sh
-. /usr/local/SAS/xmmsas_20170719_1539/setsas.sh
+. /home/ines/astrosoft/xmmsas_20180620_1732/setsas.sh
 
 cifbuild
 
@@ -84,6 +88,7 @@ org_file=$(ls $path/*$OBS*PIEVLI*)
 events_file=$path/PN_clean.fits
 gti_file=$path/PN_gti.fits
 rate_file=$path/PN_rate.fits
+img_file=$path/PN_image.fits
 
 # Creating GTI
 evselect table=$org_file withrateset=Y rateset=$rate_file maketimecolumn=Y timebinsize=100 makeratecolumn=Y expression='#XMMEA_EP && (PI in [10000:12000]) && (PATTERN==0)' -V 0
@@ -99,6 +104,11 @@ tabgtigen table=$rate_file expression="RATE<=$RATE" gtiset=$gti_file -V 0
 
 # Cleaning events file
 evselect table=$org_file withfilteredset=Y filteredset=$events_file destruct=Y keepfilteroutput=T expression="#XMMEA_EP && gti($gti_file,TIME) && (PATTERN<=4) && (PI in [500:12000])" -V 0
+
+ds9 $events_file -bin factor 64 -scale log -cmap bb -mode region &
+
+# Creating image file
+evselect table=$events_file imagebinning=binSize imageset=$img_file withimageset=yes xcolumn=X ycolumn=Y ximagebinsize=80 yimagebinsize=80 -V 0
 
 echo > $path/PN_processing.log "Rate: $RATE"
 echo "The end" 
