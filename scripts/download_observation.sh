0c79faa22865ae39d7d22c2098f77cf6a468ab28
Inés Pastor-Marazuela <29770213+InesPM@users.noreply.github.com>
2020-03-10 18:59:30 +0100

    Adding options to download MOS1 and MOS2

--- scripts/download_observation.sh
+++ scripts/download_observation.sh
@@ -2,13 +2,33 @@
 
 folder=$1
 obs=$2
+mode=(${3:-"PN"} $4 $5) # Mode event list
 path=$folder/$obs
 if [ ! -d $path ]; then mkdir $path; fi
 
 cd $path
 
+###
 # Observation
-curl -o $path/P${obs}PNS001PIEVLI.FTZ "http://nxsa.esac.esa.int/nxsa-sl/servlet/data-action-aio?obsno=${obs}&instname=PN&level=PPS&name=PIEVLI"
+###
+
+#PN
+if [[ " ${mode[@]} " =~ " PN " ]]; then
+  echo "Downloading PN data ..."
+  curl -o $path/P${obs}PNS001PIEVLI.FTZ "http://nxsa.esac.esa.int/nxsa-sl/servlet/data-action-aio?obsno=${obs}&instname=PN&level=PPS&name=PIEVLI"
+fi
+
+# MOS1
+if [[ " ${mode[@]} " =~ " M1 " ]] | [[ " ${mode[@]} " =~ " MOS " ]] ; then
+  echo "Downloading M1 data ..."
+  curl -o $path/P${obs}M1S002MIEVLI.FTZ "http://nxsa.esac.esa.int/nxsa-sl/servlet/data-action-aio?obsno=${obs}&instname=M1&level=PPS&name=MIEVLI"
+fi
+
+# MOS2
+if [[ " ${mode[@]} " =~ " M2 " ]] | [[ " ${mode[@]} " =~ " MOS " ]] ; then
+  echo "Downloading M2 data ..."
+  curl -o $path/P${obs}M2S003MIEVLI.FTZ "http://nxsa.esac.esa.int/nxsa-sl/servlet/data-action-aio?obsno=${obs}&instname=M2&level=PPS&name=MIEVLI"
+fi
 
 # Summary file
 curl -o $path/sas.TAR "http://nxsa.esac.esa.int/nxsa-sl/servlet/data-action-aio?obsno=${obs}&level=ODF&extension=SAS"
@@ -17,7 +37,3 @@ rm *ATS.FIT *TCS.FIT *RAS.ASC *ROS.ASC MANIFEST* *.TAR
 
 # Fbktsr
 curl -o $path/P${obs}PNS001FBKTSR0000.FTZ "http://nxsa.esac.esa.int/nxsa-sl/servlet/data-action-aio?obsno=${obs}&name=FBKTSR&instname=PN&level=PPS&extension=FTZ"
-
-
-
-
