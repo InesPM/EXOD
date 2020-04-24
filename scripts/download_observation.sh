4b314d99f4b7cd4d3a205b180e747158705b58d5
Inés Pastor-Marazuela <29770213+InesPM@users.noreply.github.com>
2020-04-24 12:01:07 +0200

    Fixing bug
    if conditions: replacing | by ||

--- scripts/download_observation.sh
+++ scripts/download_observation.sh
@@ -7,25 +7,25 @@ path=$folder/$obs
 if [ ! -d $path ]; then mkdir $path; fi
 
 cd $path
-
+echo ${mode[@]}
 ###
 # Observation
 ###
 
 #PN
-if [[ " ${mode[@]} " =~ " PN " ]]; then
+if [[ "${mode[@]}" =~ "PN" ]]; then
   echo "Downloading PN data ..."
   curl -o $path/P${obs}PNS001PIEVLI.FTZ "http://nxsa.esac.esa.int/nxsa-sl/servlet/data-action-aio?obsno=${obs}&instname=PN&level=PPS&name=PIEVLI"
 fi
 
 # MOS1
-if [[ " ${mode[@]} " =~ " M1 " ]] | [[ " ${mode[@]} " =~ " MOS " ]] ; then
+if [[ "${mode[@]}" =~ "M1" ]] || [[ "${mode[@]}" =~ "MOS" ]] ; then
   echo "Downloading M1 data ..."
   curl -o $path/P${obs}M1S002MIEVLI.FTZ "http://nxsa.esac.esa.int/nxsa-sl/servlet/data-action-aio?obsno=${obs}&instname=M1&level=PPS&name=MIEVLI"
 fi
 
 # MOS2
-if [[ " ${mode[@]} " =~ " M2 " ]] | [[ " ${mode[@]} " =~ " MOS " ]] ; then
+if [[ "${mode[@]}" =~ "M2" ]] || [[ "${mode[@]}" =~ "MOS" ]] ; then
   echo "Downloading M2 data ..."
   curl -o $path/P${obs}M2S003MIEVLI.FTZ "http://nxsa.esac.esa.int/nxsa-sl/servlet/data-action-aio?obsno=${obs}&instname=M2&level=PPS&name=MIEVLI"
 fi
